7d4008df4840831f4dd69160ccb810d37a16d512
Shiran Iroshan <shiraniroshan@gmail.com>
2021-01-14 00:45:59 +0530

    Updated spike_report sql file with new configurations.

--- DBSnaps/spike_report.sql
+++ DBSnaps/spike_report.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jan 12, 2021 at 07:18 AM
+-- Generation Time: Jan 13, 2021 at 07:13 PM
 -- Server version: 5.7.27-log
 -- PHP Version: 7.2.27
 
@@ -31,7 +31,6 @@ SET time_zone = "+00:00";
 CREATE TABLE `student_envolmenting_report_view` (
 `xid` char(36)
 ,`name` varchar(50)
-,`address` varchar(200)
 ,`registeredBy` varchar(36)
 ,`registeredDate` datetime
 );
@@ -43,7 +42,7 @@ CREATE TABLE `student_envolmenting_report_view` (
 --
 DROP TABLE IF EXISTS `student_envolmenting_report_view`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `student_envolmenting_report_view`  AS  select `stv`.`xid` AS `xid`,`stv`.`name` AS `name`,`stv`.`address` AS `address`,`ebv`.`registeredBy` AS `registeredBy`,`ebv`.`registeredDate` AS `registeredDate` from (`spike_registration_db`.`student_view` `stv` join `spike_enrolmenting_db`.`envolment_by_view` `ebv` on((`stv`.`xid` = `ebv`.`student_xid`))) group by `stv`.`xid` ;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `student_envolmenting_report_view`  AS  select `sv`.`xid` AS `xid`,any_value(`sv`.`name`) AS `name`,any_value(`ev`.`registeredBy`) AS `registeredBy`,any_value(`ev`.`registeredDate`) AS `registeredDate` from (`spike_registration_db`.`student_view` `sv` join `spike_enrolmenting_db`.`envolment_by_view` `ev` on((`sv`.`xid` = `ev`.`student_xid`))) group by `sv`.`xid` ;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
